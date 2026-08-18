34fde6dfe66dc4e0136ba4a9dbb0493c01155559
Caelvanost <caelvanost@gmail.com>
2026-08-18 14:54:15 +0200

    Add precompiled header

---
+++ src/PCH.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <RE/Skyrim.h>
+#include <SKSE/SKSE.h>
+#include <SKSE/Logger.h>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
