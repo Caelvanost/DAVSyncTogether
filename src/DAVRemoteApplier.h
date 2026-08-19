d9d481b20602e97a9322d9502bfc78f429c295bc
Caelvanost <caelvanost@gmail.com>
2026-08-20 00:41:03 +0200

    Apply remote state through DAV Papyrus API

--- src/DAVRemoteApplier.h
+++ src/DAVRemoteApplier.h
@@ -7,9 +7,8 @@ namespace DAVSyncTogether
     struct RemoteApplyResult
     {
         bool supported{ false };
-        std::size_t matchedNodes{ 0 };
-        std::size_t changedNodes{ 0 };
-        std::size_t headFixes{ 0 };
+        bool dispatched{ false };
+        std::size_t fallbackNodes{ 0 };
     };
 
     class DAVRemoteApplier
@@ -18,17 +17,13 @@ namespace DAVSyncTogether
         static RemoteApplyResult Apply(RE::Actor* proxyActor, const RemoteArmorState& state);
 
     private:
-        static void VisitAndApply(
+        static bool DispatchApplyVariant(RE::Actor* actor, std::string_view variant);
+        static bool DispatchResetVariant(RE::Actor* actor, RE::TESObjectARMO* armor);
+        static void FallbackCull(
             RE::NiAVObject* object,
             RE::FormID armorFormID,
             bool cull,
             RemoteApplyResult& result);
-        static void ApplyHeadHairVisibility(
-            RE::Actor* actor,
-            RE::TESObjectARMO* armor,
-            bool hidden,
-            RemoteApplyResult& result);
-
         static std::optional<std::pair<RE::FormID, RE::FormID>> ParseArmorNode(std::string_view name);
         static std::optional<RE::FormID> ParseFormID(std::string_view text);
     };
