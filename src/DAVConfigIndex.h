f66a427a1fd500809c8339d86b3231ae825270bc
Caelvanost <caelvanost@gmail.com>
2026-08-20 00:38:44 +0200

    Index DAV variants and infer active variant

--- src/DAVConfigIndex.h
+++ src/DAVConfigIndex.h
@@ -11,13 +11,30 @@ namespace DAVSyncTogether
 
         void Load();
         [[nodiscard]] bool IsArmorRelevant(const WornArmorState& armor) const;
+        [[nodiscard]] std::vector<std::string> FindMatchingVariants(const WornArmorState& armor) const;
 
     private:
+        struct VariantRule
+        {
+            std::string name;
+            std::string linkTo;
+            std::string overrideHead;
+            std::unordered_map<std::string, std::vector<FormIdentity>> replaceByForm;
+            std::unordered_map<std::uint32_t, std::vector<FormIdentity>> replaceBySlot;
+        };
+
         static std::optional<FormIdentity> ParseStableForm(std::string_view value);
         static bool ArmorAddonUsesSlot(RE::TESObjectARMA* addon, std::uint32_t slotNumber);
+        static void SortAndUnique(std::vector<FormIdentity>& values);
+
+        [[nodiscard]] std::vector<FormIdentity> BuildExpectedActive(
+            const WornArmorState& armor,
+            const VariantRule& variant,
+            bool& affected) const;
 
         std::unordered_set<std::string> _sourceArmorAddons;
         std::unordered_set<std::uint32_t> _sourceSlots;
+        std::unordered_map<std::string, VariantRule> _variants;
         bool _loaded{ false };
     };
 }
